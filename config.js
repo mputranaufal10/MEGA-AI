d2e3a6d4faff07b313b9744c9854c0daa6a4409e
mputranaufal10 <nepalnopal99@gmail.com>
2025-02-05 19:25:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ import fs from 'fs'
 import dotenv from 'dotenv'
 dotenv.config()
 
-const defaultOwner = '923444844060';
+const defaultOwner = '60196592577';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
@@ -20,8 +20,8 @@ for (let i = 0; i < ownerlist.length; i++) {
 }
 //
 global.botname = process.env.BOTNAME || 'MEGA-AI';
-global.pairingNumber = process.env.BOT_NUMBER || '' ;  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || '' ;  // put your session id here
+global.pairingNumber = process.env.BOT_NUMBER || '60196592577' ;  // put your number here
+global.SESSION_ID = process.env.SESSION_ID || 'TZwlST7Z#FLTFnmSUKnGvIN3-5FnKH0RhHd_IGfJmpRDRRdthtys' ;  // put your session id here
 
 global.mods = []
 global.prems = []
